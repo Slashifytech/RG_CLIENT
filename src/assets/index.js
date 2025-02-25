3a289d4a740dae160afd9bba1d3eee9ff1d1675c
Minhaz Ashraf <minhazashraf590@gmail.com>
2025-02-25 09:57:25 +0530

    added stamp in pdf

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,9 @@
-import logo from './logo.png';
-import close from './close.png';
-import loginBanner from './loginBanner.png'
-import menu from './menu.png';
-import error from './error.png'
-import dnf from './dnf.png'
+import logo from "./logo.png";
+import close from "./close.png";
+import loginBanner from "./loginBanner.png";
+import menu from "./menu.png";
+import error from "./error.png";
+import dnf from "./dnf.png";
 import mgpdf from "./mgpdf.png";
 import colorLogo from "./colorLogo.png";
 import greenCheck from "./greenCheck.png";
@@ -12,10 +12,22 @@ import mobile from "./mobile.png";
 import invoicelogo from "./invoicelogo.png";
 import mgLogo from "./mg.webp";
 import mgBlack from "./mgBlack.png";
+import stamp from "./stamp.jpeg";
 
-
-
-
-
-
-export {logo,mgBlack, mgLogo, close, menu, dnf, loginBanner, error, mgpdf, colorLogo, greenCheck, mobile, tlogo, invoicelogo}
+export {
+  logo,
+  mgBlack,
+  mgLogo,
+  close,
+  menu,
+  dnf,
+  loginBanner,
+  error,
+  mgpdf,
+  colorLogo,
+  greenCheck,
+  mobile,
+  tlogo,
+  invoicelogo,
+  stamp,
+};
