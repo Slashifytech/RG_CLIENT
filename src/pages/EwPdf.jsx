fce4e2a7aebd16bb4b66844e396385586afb0659
Slashifytech <theslashifytech@gmail.com>
2025-03-25 15:58:35 +0530

    Update EwPdf.jsx

--- src/pages/EwPdf.jsx
+++ src/pages/EwPdf.jsx
@@ -381,7 +381,7 @@ console.log(pageTypeAData, pageTypeBData, "dataCheck");
 )}
 
         <img src={faq} alt="header" loading="lazy" className="pt-[16%] w-[98%]" />
-        <img src={motorWarranty} alt="header" loading="lazy" className="mt-9 pt-[4%]" />
+        <img src={motorWarranty} alt="header" loading="lazy" className="mt-9 pt-[4%] w-[98%]" />
         <img src={generalTerms} alt="header" loading="lazy" className="mt-9" />
         <img src={generalCond} alt="header" loading="lazy" className="mt-9" />
       </div>
